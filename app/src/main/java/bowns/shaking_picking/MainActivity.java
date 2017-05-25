41955bc7d27fd73f1133c964c9999cdb86a9b160
Bo-Wun Shih <bowns1688@hotmail.com>
2017-05-25 08:58:28 +0800

    [add] init android project

---
+++ app/src/main/java/bowns/shaking_picking/MainActivity.java
@@ -0,0 +1,13 @@
+package bowns.shaking_picking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
